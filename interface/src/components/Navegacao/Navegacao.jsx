f2ee6993fef211a4ac0ce6e14fb8c39c536915d0
Mj <yosh6043@gmail.com>
2024-10-11 09:32:06 -0300

    Desafio 1!

--- interface/src/components/Navegacao/Navegacao.jsx
+++ interface/src/components/Navegacao/Navegacao.jsx
@@ -1,8 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/esm/Button';
+import AuthRequests from '../../fetch/AuthRequests';
+import { useState, useEffect } from 'react';
 
 function Navegacao() {
+    // criando estado para controlar a renderização condicional
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    /**
+    * Verifica a autenticação do usuário
+    */
+    useEffect(() => {
+        const token = localStorage.getItem('token');  // recupera o token do localstorage
+        if (token && AuthRequests.checkTokenExpiry()) {  // varifica a validade do token
+            setIsAuthenticated(true);  // caso o token seja válido, seta o valor de autenticação para true
+        } else {
+            setIsAuthenticated(false);  // caso o token seja inválido, seta o valor de autenticação para false
+        }
+    }, []);
 
     const estiloNavbar = {
         backgroundColor: 'var(--primaryColor)',
@@ -12,14 +29,28 @@ function Navegacao() {
         color: 'var(--fontColor)',
     }
 
+    const logout = () => {
+        AuthRequests.removeToken();
+    }
+
     return (
         <>
             <Navbar style={estiloNavbar}>
                 <Container>
+                    {/* a opção Home é renderizada para todos os usuários, independente de estarem autenticados ou não */}
                     <Navbar.Brand href="/" style={estiloNavOptions}>Home</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/pessoas" style={estiloNavOptions}>Pessoas</Nav.Link>
-                    </Nav>
+                    {isAuthenticated ? ( // verifica se o usuário está autenticado (true)
+                        // renderiza as opções de navegação para usuário autenticado
+                        <>
+                            <Nav className="me-auto">
+                                <Nav.Link href="/pessoas" style={estiloNavOptions}>Pessoas</Nav.Link>
+                            </Nav>
+                            <Button variant='light' onClick={logout}>Sair</Button>
+                        </>
+                    ) : (
+                        // renderiza as opções de navegação para usuário não autenticado
+                        <Button href='/login' variant='light'>Login</Button>
+                    )}
                 </Container>
             </Navbar>
         </>
--- interface/src/fetch/PessoaRequests.js
+++ interface/src/fetch/PessoaRequests.js
@@ -6,10 +6,19 @@ class PessoaRequests {
     }
 
     async listarPessoas() {
-        try {
-            const response = await fetch(`${this.serverUrl}${this.routeListarPessoas}`);
+        const token = localStorage.getItem('token');   // recuperando o token do localStorage
 
-            if(!response.ok) {
+        try {
+            const response = await fetch(`${this.serverUrl}${this.routeListarPessoas}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-access-token': `${token}`    // enviando o token na requisição
+                    }
+                });
+
+            if (!response.ok) {
                 throw new Error('Não foi possível listar as pessoas.');
             }
 
@@ -19,7 +28,6 @@ class PessoaRequests {
             return null;
         }
     }
-
 }
 
 export default new PessoaRequests();
